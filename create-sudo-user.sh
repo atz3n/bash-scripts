b3294f9a8dbd7e7bbbf57c66055e04fd58083a59
atzen <Atzen2@web.de>
2018-09-21 12:53:13 +0200

    removed relative paths and changed comments

--- create-sudo-user.sh
+++ create-sudo-user.sh
@@ -14,35 +14,31 @@ USER_PASSWORD="<password of new sudo user>"
 
 
 
-# add user
+echo "[INFO] creating user ${USER_NAME} ..."
 adduser ${USER_NAME} --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 
 
-# set user password
-echo ${USER_NAME}":"${USER_PASSWORD} | chpasswd
+echo "[INFO] setting user password ..."
+echo "${USER_NAME}:${USER_PASSWORD}" | chpasswd
 
 
-# add user to sudo group
+echo "[INFO] adding user to sudo group ..."
 usermod -aG sudo ${USER_NAME}
 
 
-# create users folder for ssh public keys
-mkdir ./../home/${USER_NAME}/.ssh
+echo "[INFO] copying public keys ..."
+mkdir /home/${USER_NAME}/.ssh
+cp /root/.ssh/authorized_keys /home/${USER_NAME}/.ssh/
+chown -R ${USER_NAME} /home/${USER_NAME}/.ssh/
 
-
-# copy ssh keys to public keys folder
-cp .ssh/authorized_keys ./../home/${USER_NAME}/.ssh/
-
-
-# set ownership to new user
-chown -R ${USER_NAME} ./../home/${USER_NAME}/.ssh/
-
-
-# execute commands as new user:
+# setting permissions
 # make public keys folder visible, accessible and changeable only by new user
 # make keys visible and changeable only by new user
 su -c "cd ~ ; chmod 700 .ssh ; chmod 600 .ssh/authorized_keys" "${USER_NAME}"
 
 
-# remove public key in root account (so that ssh isn't accessable via root@<IP> anymore)
-rm .ssh/authorized_keys
+echo "[INFO] removing public keys from root account (so that root isn't accessable via ssh anymore) ..."
+rm /root/.ssh/authorized_keys
+
+
+echo "[INFO] ...finished. All things are done. Close connection and login as ${USER_NAME} again ..."

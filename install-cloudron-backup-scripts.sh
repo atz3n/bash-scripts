4dfb547d4c1766b8f005e5b16deb63c02f169f37
Atzen <Atzen2@web.de>
2019-03-27 10:51:12 +0100

    changed s3cmd configuration from interactive mode to config file

--- install-cloudron-backup-scripts.sh
+++ install-cloudron-backup-scripts.sh
@@ -84,17 +84,84 @@ done
 # DEFINES
 ###################################################################################################
 
-S3CLIENT_CONFIG_CONTENT="${STORAGE_ACCESS_KEY}
-${STORAGE_SECRET_KEY}
-
-${STORAGE_ENDPOINT}
-%(bucket)s.${STORAGE_ENDPOINT}
-
-
-Yes
-
-Y
-y
+S3CLIENT_CONFIG_FILE_CONTENT="
+[default]
+access_key = ${STORAGE_ACCESS_KEY}
+access_token = 
+add_encoding_exts = 
+add_headers = 
+bucket_location = EU
+ca_certs_file = 
+cache_file = 
+check_ssl_certificate = True
+check_ssl_hostname = True
+cloudfront_host = cloudfront.amazonaws.com
+default_mime_type = binary/octet-stream
+delay_updates = False
+delete_after = False
+delete_after_fetch = False
+delete_removed = False
+dry_run = False
+enable_multipart = True
+encoding = UTF-8
+encrypt = False
+expiry_date = 
+expiry_days = 
+expiry_prefix = 
+follow_symlinks = False
+force = False
+get_continue = False
+gpg_command = /usr/bin/gpg
+gpg_decrypt = %(gpg_command)s -d --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_encrypt = %(gpg_command)s -c --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_passphrase = 
+guess_mime_type = True
+host_base = ${STORAGE_ENDPOINT}
+host_bucket = %(bucket)s.${STORAGE_ENDPOINT}
+human_readable_sizes = False
+invalidate_default_index_on_cf = False
+invalidate_default_index_root_on_cf = True
+invalidate_on_cf = False
+kms_key = 
+limit = -1
+limitrate = 0
+list_md5 = False
+log_target_prefix = 
+long_listing = False
+max_delete = -1
+mime_type = 
+multipart_chunk_size_mb = 15
+multipart_max_chunks = 10000
+preserve_attrs = True
+progress_meter = True
+proxy_host = 
+proxy_port = 0
+put_continue = False
+recursive = False
+recv_chunk = 65536
+reduced_redundancy = False
+requester_pays = False
+restore_days = 1
+restore_priority = Standard
+secret_key = ${STORAGE_SECRET_KEY}
+send_chunk = 65536
+server_side_encryption = False
+signature_v2 = False
+signurl_use_https = False
+simpledb_host = sdb.amazonaws.com
+skip_existing = False
+socket_timeout = 300
+stats = False
+stop_on_error = False
+storage_class = 
+urlencoding_mode = normal
+use_http_expect = False
+use_https = True
+use_mime_magic = True
+verbosity = WARNING
+website_endpoint = http://%(bucket)s.s3-website-%(location)s.amazonaws.com/
+website_error = 
+website_index = index.html
 "
 
 
@@ -259,9 +326,7 @@ echo "[INFO] Installing s3 client ..."
 sudo apt install -y s3cmd
 
 echo "[INFO] Configuring s3 client ..."
-echo "${S3CLIENT_CONFIG_CONTENT}" > s3-config.txt
-s3cmd --configure < s3-config.txt
-rm s3-config.txt
+echo "${S3CLIENT_CONFIG_FILE_CONTENT}" > .s3cfg
 
 
 echo "[INFO] creating backup pulling file ..."

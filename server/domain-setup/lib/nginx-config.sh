8f1a24577df2c0e90463d6b70db8e9cdc9c7ba61
atz3n <A_Aldemir@hotmail.de>
2022-02-23 14:16:11 +0100

    add websocket support

--- server/domain-setup/lib/nginx-config.sh
+++ server/domain-setup/lib/nginx-config.sh
@@ -6,18 +6,20 @@
 
 DOMAIN="<new domain>"
 PORT="<new internal port mapping>"
+ALLOW_WEBSOCKET=false
 
 
 ###################################################################################################
 # PARAMETER PARSING
 ###################################################################################################
 
-while getopts "h?p:d:" opt; do
+while getopts "h?w?p:d:" opt; do
     case "$opt" in
     h)
         echo "Parameter:"
         echo "-d  <new domain>"
         echo "-p  <new internal port mapping>"
+        echo "-w  (allow websocket connection)"
         exit 0
         ;;
     d)  
@@ -25,6 +27,9 @@ while getopts "h?p:d:" opt; do
         ;;
     p)  
         PORT=$OPTARG
+        ;;
+    w)  
+        ALLOW_WEBSOCKET=true
         ;;
     esac
 done
@@ -48,13 +53,37 @@ server {
 "
 
 
+NGINX_LETS_ENCRYPT_GATEWAY_WITH_WEBSOCKET_CONFIGURATION_FILE_CONTENT="
+server {
+    server_name ${DOMAIN};
+    listen 80;
+
+    location / {
+        proxy_bind 127.0.0.1;
+        proxy_pass http://127.0.0.1:${PORT}/;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \"Upgrade\";
+        proxy_set_header Host \$host;
+        proxy_read_timeout 86400s;
+        proxy_send_timeout 86400s;
+    }
+}
+"
+
+
 ###################################################################################################
 # MAIN
 ###################################################################################################
 
 echo "[INFO] creating gateway ..."
 sudo service nginx stop
-echo "${NGINX_LETS_ENCRYPT_GATEWAY_CONFIGURATION_FILE_CONTENT}" | sudo tee /etc/nginx/conf.d/${DOMAIN}.conf > /dev/null
+
+if [ ${ALLOW_WEBSOCKET} == true ]; then
+    echo "${NGINX_LETS_ENCRYPT_GATEWAY_WITH_WEBSOCKET_CONFIGURATION_FILE_CONTENT}" | sudo tee /etc/nginx/conf.d/${DOMAIN}.conf > /dev/null
+else 
+    echo "${NGINX_LETS_ENCRYPT_GATEWAY_CONFIGURATION_FILE_CONTENT}" | sudo tee /etc/nginx/conf.d/${DOMAIN}.conf > /dev/null
+fi
 
 
 echo "" && echo "[INFO] requesting Let's Encrypt certificate(s) ..."
--- server/domain-setup/setup-domain.sh
+++ server/domain-setup/setup-domain.sh
@@ -9,18 +9,21 @@ USER_NAME="<sudo user name>"
 DOMAIN="<application domain>"
 APPLICATION_PORT="<application port>"
 
+ALLOW_WEBSOCKET=false
+
 
 ###################################################################################################
 # PARAMETER PARSING
 ###################################################################################################
 
-while getopts "h?p:d:u:" opt; do
+while getopts "h?w?p:d:u:" opt; do
     case "$opt" in
     h)
         echo "Parameter:"
         echo "-d  <new application domain>"
         echo "-p  <new application port>"
         echo "-u  <sudo user name>"
+        echo "-w  (allow websocket connection)"
         exit 0
         ;;
     d)  
@@ -31,6 +34,9 @@ while getopts "h?p:d:u:" opt; do
         ;;
     u)  
         USER_NAME=$OPTARG
+        ;;
+    w)  
+        ALLOW_WEBSOCKET=true
         ;;
     esac
 done
@@ -44,9 +50,11 @@ done
 # MAIN
 ###################################################################################################
 
+WF=""
+if [ ${ALLOW_WEBSOCKET} == true ]; then WF="-w"; fi
 
 scp ./lib/nginx-config.sh ${USER_NAME}@${DOMAIN}:
-ssh -t ${USER_NAME}@${DOMAIN} "./nginx-config.sh -d ${DOMAIN} -p ${APPLICATION_PORT}"
+ssh -t ${USER_NAME}@${DOMAIN} "./nginx-config.sh -d ${DOMAIN} -p ${APPLICATION_PORT} ${WF}"
 ssh -t ${USER_NAME}@${DOMAIN} "rm nginx-config.sh"
 
 echo "" && echo "[INFO] Done. Domain set up."

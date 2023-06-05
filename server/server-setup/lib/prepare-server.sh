6cf564d1faac48dfacf16435aae0b79c6480c0cf
atz3n <attila.aldemir@atz3n.dev>
2023-06-05 10:15:25 +0200

    fix typo

--- server/server-setup/lib/prepare-server.sh
+++ server/server-setup/lib/prepare-server.sh
@@ -56,7 +56,7 @@ LOCAL_USER=$(whoami)
 
 PROFILE_LANGUAGE="
 export LANGUAGE=\"en_US.UTF-8\"
-export LANG=\"en_US.UTF-8 \"
+export LANG=\"en_US.UTF-8\"
 export LC_ALL=\"en_US.UTF-8\"
 export LC_CTYPE=\"en_US.UTF-8\"
 "
